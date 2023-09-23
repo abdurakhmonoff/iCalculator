6b4e865f8a984218079e703542c1e2ef0223940f
abdurakhmonoff <a.abdurakhmonoff@gmail.com>
2023-09-23 20:13:24 +0500

    Add functionality to the operations and the equal button

--- iCalculator/ContentView.swift
+++ iCalculator/ContentView.swift
@@ -19,7 +19,7 @@ enum CalcButton: String {
     case nine = "9"
     case zero = "0"
     case add = "+"
-    case substract = "-"
+    case subtract = "-"
     case divide = "÷"
     case multiply = "×"
     case equal = "="
@@ -30,7 +30,7 @@ enum CalcButton: String {
     
     var buttonColor: Color {
         switch self {
-        case .add, .substract, .multiply, .divide, .equal:
+        case .add, .subtract, .multiply, .divide, .equal:
             return Color(UIColor(red: 230/255.0, green: 126/255.0, blue: 34/255.0, alpha: 1))
         case .clear, .negative, .percent:
             return Color(.darkGray)
@@ -40,14 +40,20 @@ enum CalcButton: String {
     }
 }
 
+enum Operation {
+    case add, subtract, multiply, divide, none
+}
+
 struct ContentView: View {
     
     @State var value = "0"
+    @State var runningNumber = 0
+    @State var currentOperation: Operation = .none
     
     let buttons: [[CalcButton]] = [
         [.clear, .negative, .percent, .divide],
         [.seven, .eight, .nine, .multiply],
-        [.four, .five, .six, .substract],
+        [.four, .five, .six, .subtract],
         [.one, .two, .three, .add],
         [.zero, .decimal, .equal],
     ]
@@ -64,7 +70,7 @@ struct ContentView: View {
                     Spacer()
                     Text(value)
                         .bold()
-                        .font(.system(size: 72))
+                        .font(.system(size: 80))
                         .foregroundColor(.white)
                 }
                 .padding()
@@ -74,7 +80,7 @@ struct ContentView: View {
                     HStack(spacing: 12) {
                         ForEach(row, id: \.self) { item in
                             Button(action: {
-                                
+                                self.didTap(button: item)
                             }, label: {
                                 Text(item.rawValue)
                                     .font(.system(size: 32))
@@ -87,6 +93,56 @@ struct ContentView: View {
                     }
                 }
                 .padding(.bottom, 3)
+            }
+        }
+    }
+    
+    func didTap(button: CalcButton) {
+        switch button {
+        case .add, .subtract, .multiply, .divide, .equal:
+            if button == .add {
+                self.currentOperation = .add
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .subtract {
+                self.currentOperation = .subtract
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .multiply {
+                self.currentOperation = .multiply
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .divide {
+                self.currentOperation = .divide
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .equal {
+                let runningValue = self.runningNumber
+                let currentValue = Int(self.value) ?? 0
+                switch self.currentOperation {
+                case .add: self.value = "\(runningValue + currentValue)"
+                case .subtract: self.value = "\(runningValue - currentValue)"
+                case .multiply: self.value = "\(runningValue * currentValue)"
+                case .divide: self.value = "\(runningValue / currentValue)"
+                case .none:
+                    break
+                }
+            }
+            
+            if button != .equal {
+                self.value = "0"
+            }
+        case .clear:
+            self.value = "0"
+        case .decimal, .negative, .percent:
+            break
+        default:
+            let number = button.rawValue
+            if self.value == "0" {
+                value = number
+            }
+            else {
+                self.value = "\(self.value)\(number)"
             }
         }
     }
