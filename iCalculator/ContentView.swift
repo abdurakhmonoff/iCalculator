f8b31dc5929c5d87a049e9d0558ff689cbb017bf
abdurakhmonoff <a.abdurakhmonoff@gmail.com>
2023-09-23 19:34:41 +0500

    Create the calculator interface

--- iCalculator/ContentView.swift
+++ iCalculator/ContentView.swift
@@ -7,16 +7,100 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+enum CalcButton: String {
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case zero = "0"
+    case add = "+"
+    case substract = "-"
+    case divide = "÷"
+    case multiply = "×"
+    case equal = "="
+    case clear = "AC"
+    case decimal = "."
+    case percent = "%"
+    case negative = "-/+"
+    
+    var buttonColor: Color {
+        switch self {
+        case .add, .substract, .multiply, .divide, .equal:
+            return Color(UIColor(red: 230/255.0, green: 126/255.0, blue: 34/255.0, alpha: 1))
+        case .clear, .negative, .percent:
+            return Color(.darkGray)
+        default:
+            return Color(UIColor(red: 44/255.0, green: 62/255.0, blue: 80/255.0, alpha: 1))
         }
-        .padding()
-    } 
+    }
+}
+
+struct ContentView: View {
+    
+    @State var value = "0"
+    
+    let buttons: [[CalcButton]] = [
+        [.clear, .negative, .percent, .divide],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .substract],
+        [.one, .two, .three, .add],
+        [.zero, .decimal, .equal],
+    ]
+    
+    var body: some View {
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                
+                // Text display
+                HStack {
+                    Spacer()
+                    Text(value)
+                        .bold()
+                        .font(.system(size: 72))
+                        .foregroundColor(.white)
+                }
+                .padding()
+                
+                // The buttons
+                ForEach(buttons, id: \.self) { row in
+                    HStack(spacing: 12) {
+                        ForEach(row, id: \.self) { item in
+                            Button(action: {
+                                
+                            }, label: {
+                                Text(item.rawValue)
+                                    .font(.system(size: 32))
+                                    .frame(width: self.buttonWidth(item: item), height: self.buttonHeight())
+                                    .background(item.buttonColor)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(self.buttonWidth(item: item)/2)
+                            })
+                        }
+                    }
+                }
+                .padding(.bottom, 3)
+            }
+        }
+    }
+    
+    func buttonWidth(item: CalcButton) -> CGFloat {
+        if item == .zero {
+            return ((UIScreen.main.bounds.width - (4 * 12)) / 4) * 2
+        }
+        return (UIScreen.main.bounds.width - (5 * 12)) / 4
+    }
+    
+    func buttonHeight() -> CGFloat {
+        return (UIScreen.main.bounds.width - (5 * 12)) / 4
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
