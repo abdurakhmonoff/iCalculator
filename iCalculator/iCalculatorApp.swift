8ecdeb401203a31288d0373871005b706edf7093
abdurakhmonoff <a.abdurakhmonoff@gmail.com>
2023-09-23 18:29:09 +0500

    Initial Commit

---
+++ iCalculator/iCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCalculatorApp.swift
+//  iCalculator
+//
+//  Created by Asror Abdurakhmanov on 23/09/23.
+//
+
+import SwiftUI
+
+@main
+struct iCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
